fe02f6eea28d752d04941af8bc4936e2ff48d33b
Shakir Bhat <shakir.bhat@DC-M7W2PC414Q.local>
2025-05-17 18:44:01 +0530

    variables

--- Variables/variable.sh
+++ Variables/variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#1 variable
 #define variable
 
 name="shakir"
@@ -9,3 +9,12 @@ age=10
 #access the value using dollar sign $
 
 echo " my name is $name and age is $age "
+
+
+#2 store output of command in variable
+
+HOSTNAME=$(hostname)
+echo "name of this machine is $HOSTNAME"
+
+
+
