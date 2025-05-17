393d058dafd9dd7a48bb5bc2a6003861a7cb1d77
Shakir Bhat <shakir.bhat@DC-M7W2PC414Q.local>
2025-05-17 18:22:32 +0530

    bash

---
+++ Comments/comments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+echo "Single Line Comments"
+# single line comments are not printable or nonexecutable lines starting with hash 
+
+echo "example ls command"  # ls command is used to list the files or folders
+
+echo "Multi Line Comments"
+
+<< comment
+this 
+is 
+a 
+multi
+line
+comment
+
+
+
---
+++ Variables/variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#define variable
+
+name="shakir"
+age=10
+
+#access the value using dollar sign $
+
+echo " my name is $name and age is $age "
